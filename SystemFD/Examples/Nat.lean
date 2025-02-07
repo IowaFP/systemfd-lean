599498febf58a9263a4b11c762aa6baac2ba29fb
Apoorv Ingle <apoorv-ingle@uiowa.edu>
2025-02-07 14:12:18 -0600

    - Nat example. Compare 2 nats for equality

--- SystemFD/Examples/Nat.lean
+++ SystemFD/Examples/Nat.lean
@@ -31,42 +31,69 @@ def NatCtxFix : Ctx Term := [
   .datatype ★
 ]
 
-def NatCtxDirect : Ctx Term := [
-  -- let two : Nat := succ (succ zero)
-  .term #4 (#2 `@ (#2 `@ #3)),
-  -- instance add : λ n.
-  --   ite zero <- n then λ m. m
-  --     else ite succ <- n then λ x. λ m. succ (add x m)
-  --     else m
-  .inst 0 (`λ[#3]
-    .ite #3 #0 (`λ[#4] #0) (
-      .ite #2 #0 (`λ[#4] `λ[#5] #4 `@ (#3 `@ #1 `@ #0))
-      (`λ[#4] #0))),
-  -- open add : Nat -> Nat -> Nat
-  .openm (#2 -t> #3 -t> #4),
-  -- Nat.succ
-  .ctor (#1 -t> #2),
-  -- Nat.zero
-  .ctor #0,
-  -- Nat
-  .datatype ★
-]
 
 #eval wf_ctx NatCtxFix
 #eval infer_type NatCtxFix (#1 `@ #0 `@ #0)
 
-#eval infer_type NatCtxDirect (#2 `@ #0 `@ #0)
-#eval infer_type NatCtxDirect #2
-
--- #eval instance_indices NatCtxDirect 0 2
--- #eval instance_indices' NatCtxFix 0 4 []
-#eval wf_ctx NatCtxDirect
-unsafe def tfinal : List Term := eval_ctx_loop NatCtxFix (#1 `@ #0 `@ #0)
-#eval tfinal
-
-
 #eval infer_type NatCtxFix (((#4 `@t (#7 -t> #8 -t> #9)) `@ #2) `@ (#5 `@ #6) `@ #0)
 #eval eval_ctx_loop NatCtxFix (((#4 `@t (#7 -t> #8 -t> #9)) `@ #2) `@ (#5 `@ #6) `@ #0)
 
-#eval eval_inst NatCtxDirect (#2 `@ #0 `@ #0)
-#eval eval_ctx_loop NatCtxDirect (#2 `@ #0 `@ #0)
+def NatCtxDirect : Ctx Term := [
+
+    /- ==@Nat := λ x λ y.
+                  case x of
+                    Zero → case y of
+                            Zero → True
+                            _ → False
+                    Suc → λ x'. case y of
+                            Zero → False
+                            Succ →  λ y'. ==@Nat x' y'
+   -/
+   .inst 0 ( `λ[#9] `λ[#10]
+      .ite #10 #1 (.ite #10 #0 #3 -- x = 0; y = 0
+                               #4 -- x = 0; y ≠ 0
+                                )
+      (.ite #9 #1 (`λ[#11]
+                    .ite #11 #1 #5 -- x = S n; y = 0
+                   (.ite #10 #1 (`λ[#12] #4 `@ #1 `@ #0)  -- -- x = S n; y = S n'
+                                #5))
+           #4))
+
+   -- ==@Nat : Nat → Nat → Bool
+  , .openm (#8 -t> #9 -t> #4)
+
+  , .ctor #1 -- True
+  , .ctor #0 -- False
+  , .datatype ★ -- Bool
+
+  -- let two : Nat := succ (succ zero)
+  , .term #4 (#2 `@ (#2 `@ #3))
+  -- instance add : λ n.
+  --   ite zero <- n then λ m. m
+  --     else ite succ <- n then λ x. λ m. succ (add x m)
+  --     else m
+  , .inst 0 (`λ[#3]
+    .ite #3 #0 (`λ[#4] #0) (
+      .ite #2 #0 (`λ[#4] `λ[#5] #4 `@ (#3 `@ #1 `@ #0))
+      (`λ[#4] #0)))
+  -- open add : Nat -> Nat -> Nat
+  , .openm (#2 -t> #3 -t> #4)
+  -- Nat.succ
+  , .ctor (#1 -t> #2)
+  -- Nat.zero
+  , .ctor #0
+  -- Nat
+  , .datatype ★
+]
+
+#eval wf_ctx NatCtxDirect
+#eval infer_type NatCtxDirect (#7 `@ #5 `@ #5)
+
+#eval eval_ctx_loop NatCtxDirect (#7 `@ #5 `@ #5)
+
+#eval infer_type NatCtxDirect (#1 `@ #5 `@ #5)
+#eval infer_type NatCtxDirect (#1 `@ #5 `@ (#8 `@ #9))
+
+#eval eval_ctx_loop NatCtxDirect (#1 `@ (#7 `@ #5 `@ #5) `@ (#7 `@ #5 `@ #5)) -- (2 + 2) =? (2 + 2)
+
+#eval eval_ctx_loop NatCtxDirect (#1 `@ #5 `@ (#8 `@ #9))
