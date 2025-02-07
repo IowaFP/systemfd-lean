150539c4add5cfdf8d5793c1ff36fe96922d9b06
Andrew Marmaduke <marmaduke.andrew@gmail.com>
2025-02-07 14:22:32 -0600

    add fundeps example

---
+++ SystemFD/Examples/Fundeps.lean
@@ -0,0 +1,36 @@
+import SystemFD.Term
+import SystemFD.Judgment
+import SystemFD.Ctx
+import SystemFD.Algorithm
+import SystemFD.Evaluator
+
+def FundepsCtx : Ctx Term := [
+  -- instance fdEqual2 = Λ t t' u. λ i1 i2.
+  --     guard EqualTT[t, u] <- i1 then λ c1.
+  --     guard EqualTT[t', u] <- i2 then λ c2.
+  --         c1 ; sym c2
+  .inst 2 (Λ[★] Λ[★] Λ[★] `λ[#7 `@k #2 `@k #0] `λ[#8 `@k #2 `@k #1]
+    .guard (#6 `@t #4 `@t #2) #1 (`λ[#4 ~ #2]
+      .guard (#7 `@t #4 `@t #3) #1 (`λ[#4 ~ #3]
+        #1 `; sym! #0
+  ))),
+  -- instance fdEqual1 = Λ t u u'. λ i1 i2.
+  --     guard EqualTT[t, u] <- i1 then λ c1.
+  --     guard EqualTT[t, u'] <- i2 then λ c2.
+  --         sym c1 ; c2
+  .inst 2 (Λ[★] Λ[★] Λ[★] `λ[#6 `@k #2 `@k #1] `λ[#7 `@k #3 `@k #1]
+    .guard (#5 `@t #4 `@t #3) #1 (`λ[#4 ~ #3]
+      .guard (#6 `@t #5 `@t #3) #1 (`λ[#5 ~ #3]
+        sym! #1 `; #0
+  ))),
+  -- instance EqualTT :: ∀ t u. t ~ u -> Equal t u
+  .insttype (∀[★] ∀[★] #1 ~ #0 -t> #5 `@k #2 `@k #1),
+  -- open fdEqual2 :: ∀ t t' u. Equal t u -> Equal t' u -> t ~ t'
+  .openm (∀[★] ∀[★] ∀[★] (#4 `@k #2 `@k #0) -t> (#5 `@k #2 `@k #1) -t> #4 ~ #3),
+  -- open fdEqual1 :: ∀ t u u'. Equal t u -> Equal t u' -> u ~ u'
+  .openm (∀[★] ∀[★] ∀[★] (#3 `@k #2 `@k #1) -t> (#4 `@k #3 `@k #1) -t> #3 ~ #2),
+  -- open Equal :: ★ -> ★ -> ◯
+  .opent (★ -k> ★ -k> ◯)
+]
+
+#eval wf_ctx FundepsCtx
