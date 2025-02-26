019d696f20c3a3b49fe2608ea05d87b6d6550f7b
Apoorv Ingle <apoorv-ingle@uiowa.edu>
2025-02-26 13:40:12 -0600

    type function example (almost complete)

--- SystemFD/Examples/TypeFunction.lean
+++ SystemFD/Examples/TypeFunction.lean
@@ -10,22 +10,40 @@ def ctx : Ctx Term := [
     --   = Λ t λd.
     --     let h = fdF[Int][Bool][t](FIB [Int][Bool] <Int> <Bool>) d in
     --     not ▹ <→> `@c h `@c h
-    .term (∀[★] (#8 `@k #9 `@k #0) -t> #1 -t> #2)
-      (Λ[★]`λ[#8 `@k #9 `@k #0] #0
+    .term (∀[★] (#9 `@k #10 `@k #9) -t> #1 -t> #2)
+      (Λ[★]`λ[#8 `@k #9 `@k #0]
+        Term.letterm ((#15 ~ #1)) (#10 `@t #12 `@t #15 `@t  #1 `@ (#11 `@t #12 `@t #15 `@t #1) `@ #0) (
+        #2 ▹ (#0 -c> #0))
       )
+
+    --  not : Bool → Bool → Bool
+    --      = λ x y. if x ← True then if y ← True then True
+    --                                else if x ← False then True
+    --                                else False
+   , .term (#12 -t> #13 -t> #14)
+           (`λ[#12]`λ[#13]
+             Term.ite #10 #1 (Term.ite #10 #0 #10
+                             (Term.ite #11 #1 #10 #11))
+                             (#11))
 
   --   Λ t u v. λ d1 d2.
   --     If FMM[t][u] ← d1 then Λ a' b'. λ h1 h2 e1.
   --     If FMM[t][v] ← d2 then Λ a'' b''. λ k1 k2 e2.
-  --     let j : (a' ~ a'') (h1 ; sym k1).2 in
+  --     let j : (a' ~ a'') = (h1 ; sym k1).2 in
   --     let e1' : F a'' b = e1 ▹ <Maybe> `@c j `@c <b'> in
   --       sym h2 ; <Maybe> `@c fdF[a''][b'][b''] e1' e2 ; k2
-  , .inst #6 (Λ[★]Λ[★]Λ[★]  `λ[#10 `@k #2 `@k  #1]  `λ[#11 `@k #3 `@k #2]
-          #0
-          )
+  , .inst #6
+      (Λ[★]Λ[★]Λ[★]  `λ[#10 `@k #2 `@k  #1]  `λ[#11 `@k #3 `@k #2]
+        Term.guard  (#5 `@k #4 `@k #3)  #1 (Λ[★]Λ[★]`λ[(#6 `@k #1) ~ #3]`λ[(#7 `@k #1) ~ #3]`λ[(#12 `@k #3 `@k #2)]
+        Term.guard  (#5 `@k #4 `@k #3) #5 (Λ[★]Λ[★] `λ[(#11 `@k #6) ~ #8]`λ[(#13`@k #7) ~ #9]`λ[(#19 `@k #10 `@k #9)]
+        Term.letterm (#9 ~ #4) (#7 `; sym! #2) (
+        Term.letterm (#23 `@k #5 `@k #9) (#6 ▹ (∀c[★]refl! #24  `@c #1 `@c (refl! #5))) (
+        sym! #8 `; (refl! #19 -c> #24 `@k #5 `@k #10 `@k #6) `; #3
+            )))))
 
   -- FMM : ∀ a b a' b'. Maybe a' ~ a → Maybe b' ~ b → F a' b' → F a b
-  , .insttype (∀[★]∀[★]∀[★]∀[★] (#6 `@k #1 ~ #3) -t> (#7 `@k #1 ~ #3) -t> (#12 `@k #3 `@k #2) -t> (#13 `@k #6 `@k #5))
+  , .insttype (∀[★]∀[★]∀[★]∀[★]
+                (#6 `@k #1 ~ #3) -t> (#7 `@k #1 ~ #3) -t> (#12 `@k #3 `@k #2) -t> (#13 `@k #6 `@k #5))
 
   -- Just : ∀ a. a → Maybe a
   , .ctor (∀[★] #0 -t> (#3 `@k #1))
@@ -47,7 +65,7 @@ def ctx : Ctx Term := [
   -- FIB : ∀ t u. Int ~ t → Bool ~ u → F t u
   , .insttype (∀[★]∀[★] (#4 ~ #1) -t> (#8 ~ #1) -t> (#5 `@k #3 `@k #2))
 
-  -- fdF : ∀ t u v. F t u → F t v → u ~ v
+  -- -- fdF : ∀ t u v. F t u → F t v → u ~ v
   , .openm (∀[★]∀[★]∀[★] (#3 `@k #2 `@k #1) -t> (#4 `@k #3 `@k #1) -t> (#3 ~ #2))
 
   -- F : ★ → ★ → ★
@@ -63,9 +81,12 @@ def ctx : Ctx Term := [
 
 #eval wf_ctx ctx
 
-
-
-
+-- #eval infer_type ctx (#0)
+-- #eval infer_kind ctx (∀[★]∀[★]∀[★] (#3 `@k #2 `@k #1) -t> (#4 `@k #3 `@k #1) -t> (#3 ~ #2))
+-- #eval infer_type ctx (Λ[★]Λ[★]Λ[★] `λ[#5 `@k #2 `@k  #1]  `λ[#6 `@k #3 `@k #1]
+--                           (Term.guard (#5 `@k #4 `@k #3) #1 (`λ[#8 ~ #4] `λ[#12 ~ #4]
+--                           Term.guard (#7 `@k #6 `@k #4) #2 (`λ[#10 ~ #6] `λ[#14 ~ #5] ((sym! #2) `; (#0)))))
+--                      )
 -- /-
 -- not : Bool -> Bool
 -- not = λ x → case x of
