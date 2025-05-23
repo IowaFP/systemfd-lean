eff7ab3934f953c63629a291a3d409f7fb1cd605
Apoorv Ingle <apoorv-ingle@uiowa.edu>
2025-05-23 12:21:31 -0500

    mapping rules added in evaluator

--- SystemFD/Evaluator/SmallStep.lean
+++ SystemFD/Evaluator/SmallStep.lean
@@ -22,6 +22,7 @@ def eval_inst (Γ : Ctx Term) : (t : Term) -> Option Term
 -- Case matching
 ------------------------
 | .ite _ `0 _ _ => .some `0
+| .ite p (.ctor2 .choice s1 s2) i t => .some (.ite p s1 i t ⊕ .ite p s2 i t)
 | .ite p s b c => do
   let (h, sp) <- Term.neutral_form p
   if Γ.is_ctor h
@@ -48,6 +49,7 @@ def eval_inst (Γ : Ctx Term) : (t : Term) -> Option Term
 -- Guards over open terms
 ------------------------
 | .guard _ `0 _ => .some `0
+| .guard p (.ctor2 .choice s1 s2) i => .some (.guard p s1 i ⊕ .guard p s2 i)
 | .guard p s c => do
   let (p', sp) <- Term.neutral_form p
   if Γ.is_insttype p'
@@ -71,45 +73,57 @@ def eval_inst (Γ : Ctx Term) : (t : Term) -> Option Term
   else .none
 
 
+--------------------------
+--- Ctors1
+---------------------------
+
 | .ctor1 _ `0 => .some `0
+| .ctor1 v (.ctor2 .choice t1 t2) => .some (.ctor2 .choice (.ctor1 v t1) (.ctor1 v t2))
 | .ctor1 _ (.ctor2 .refl K t) => .some (refl! K t)
 | .ctor1 v η => do
   let η' <- eval_inst Γ η
   .some (.ctor1 v η')
 
+--------------------------
+--- Ctors2
+---------------------------
+
 | .ctor2 .choice `0 t2 => .some t2
 | .ctor2 .choice t1 `0 => .some t1
-| .ctor2 .choice t1 t2 => -- for now, go left to right on choice, get stuck if left choice evals to non-zero
+| .ctor2 .choice t1 t2 => -- for now, go left to right on choice
   match eval_inst Γ t1 with
   | .some t1' => .some (t1' ⊕ t2)
   | .none => match eval_inst Γ t2 with
              | .some t2' => .some (t1 ⊕ t2')
              | .none => .none
 
-| .ctor2 _ `0 _ => .some `0
-| .ctor2 _ _ `0 => .some `0
-
-
 --- Evaluate second component first
 | .ctor2 .cast t (.ctor2 .refl _ _) => .some t
+| .ctor2 .cast _ `0 => .some `0
+| .ctor2 .cast f (.ctor2 .choice a1 a2) => .some (.ctor2 .choice (.ctor2 .cast f a1) (.ctor2 .cast f a2))
 | .ctor2 .cast t η => do
   let η' <- eval_inst Γ η
   .some (t ▹ η')
 
 | .ctor2 .fst K1 (.ctor2 .refl K2 (.ctor2 .appk A _)) =>
   .some (refl! (K1 -k> K2) A)
+| .ctor2 .fst _ `0 => .some `0
+| .ctor2 .fst f (.ctor2 .choice a1 a2) => .some (.ctor2 .choice (.ctor2 .fst f a1) (.ctor2 .fst f a2))
 | .ctor2 .fst K η => do
    let η' <- eval_inst Γ η
    .some (fst! K η')
 
 | .ctor2 .snd K1 (.ctor2 .refl _ (.ctor2 .appk _ B)) =>
   .some (refl! K1 B)
+| .ctor2 .snd _ `0 => .some `0
+| .ctor2 .snd f (.ctor2 .choice a1 a2) => .some (.ctor2 .choice (.ctor2 .snd f a1) (.ctor2 .snd f a2))
 | .ctor2 .snd K1 η => do
    let η' <- eval_inst Γ η
    .some (snd! K1 η')
 
-
 --- Evaluate first component first
+| .ctor2 _ `0 _ => .some `0
+| .ctor2 v (.ctor2 .choice f1 f2) a => .some (.ctor2 .choice (.ctor2 v f1 a) (.ctor2 v f2 a))
 
 | .ctor2 .app (.bind2 .lam _ b) t => .some (b β[ t ])
 | .ctor2 .app f t => do
@@ -180,10 +194,13 @@ def eval_inst (Γ : Ctx Term) : (t : Term) -> Option Term
   let t1' <- eval_inst Γ t1
   .some (.ctor2 v t1' t2)
 
-| .bind2 _ `0 _ => .some `0
-| .bind2 _ _ `0 => .some `0
-
+--------------------------
+--- Binders
+---------------------------
+-- Evaluate first component first
 | .bind2 .arrowc (.ctor2 .refl ★ t) (.ctor2 .refl ★ t') => .some (refl! ★ (t -t> t'))
+| .bind2 .arrowc `0 _ => .some `0
+| .bind2 .arrowc (.ctor2 .choice t1 t2) a => .some (.ctor2 .choice (.bind2 .arrowc t1 a) (.bind2 .arrowc t2 a))
 | .bind2 .arrowc (.ctor2 .refl ★ t) η => do
   let η' <- eval_inst (.empty :: Γ) η
   .some ((refl! ★ t) -c> η')
@@ -191,9 +208,15 @@ def eval_inst (Γ : Ctx Term) : (t : Term) -> Option Term
   let η'' <- eval_inst Γ η
   .some (η'' -c> η')
 
+
+-- Evaluate second component first
 | .bind2 .allc t (.ctor2 .refl K t') => .some (refl! K (∀[t] t'))
+| .bind2 .allc _ `0 => .some `0
+| .bind2 .allc t (.ctor2 .choice a1 a2) => .some (.ctor2 .choice (.bind2 .allc t a1) (.bind2 .arrowc t a2))
 | .bind2 .allc t η => do
   let η' <- eval_inst (.kind t :: Γ) η
   .some (∀c[t] η')
+
+-- CAUTION: No catch-all cases here for mapping rules as we do not evaluate under λ and Λ
 
 | _ => .none
