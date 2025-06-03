9b9893b013a833d21b7a01bd2f8ce46a5cc9820b
Andrew Marmaduke <marmaduke.andrew@gmail.com>
2025-06-03 15:17:36 -0500

    add plan for synthesis

---
+++ Hs/SynthInstance.lean
@@ -0,0 +1,58 @@
+
+-- Problem 1: filling in a hole
+-- There is a goal that is an instance, say `C a b c`
+-- We need to synthesize the evidence of this instance
+-- Suppose the problem looks like this:
+
+-- `class A; class B; class C; f : ∀ b c, B b c -> A b; g : ∀ a b c, A a -> C a b c; a; b; c; x : B a b ⊢ ? : C a b c`
+
+-- First lets change the context to compute the telescopes:
+-- `class A; class B; class C; f : (A b, [b, c, B b c]); g : (C a b c, [a, b, c, A a]); a; b; c; x : B a b ⊢ ? : C a b c`
+
+-- Now we will exhaustively try every contextual telescope that matches the goal, in this case this is only one:
+-- `Γ ⊢ g ?1 ?2 ?3 ?4` this produces four new goals, but it forces a = a, b = b, and c = c in the telescope
+
+-- That means that `?1 = a`, `?2 = b`, and `?3 = c`, hence:
+-- `Γ ⊢ g a b c ?4` with the goal: `Γ ⊢ ?4 : A a`
+
+-- Now we recurse, exhaustively trying every contextual telescope that matches the goal, in this case this is only one:
+-- `Γ ⊢ f ?5 ?6 ?7` this produces three new goals, but it forces b = a in the telescope
+
+-- That means `?5 = a` giving us: `Γ ⊢ f a ?6 ?7`
+-- Now we work on the _largest_ goal because it has the best chance of forcing other goals
+-- We have: `Γ ⊢ ?7 : B a c` (note this c is indexed in the telescope, not the context)
+
+-- Repeating the process again we get: `?7 = x` where `a = a` matches in the context, and it forces `c = b`
+-- This gives us: `Γ ⊢ f a b x : A a` and finally `Γ ⊢ g a b c (f a b x) : C a b c`
+
+
+
+
+
+-- Problem 2: producing a coercion
+-- There is a term with a type `u : U` but the goal type is `V`
+-- We need to try and find a coercion term `t : U ~ V` so that we can produce `u ▹ t : V`
+-- The approach is two fold: refine the goal based off the structure of the coercion we need
+--    and construct a graph of all possible coercions to search for a path from one type to another
+
+-- For example, if we need `t : U1 -> U2 ~ V1 -> V2`, then we should immediately use the arrow coercion rule:
+-- `?1 -c> ?2` with `?1 : U1 ~ V1` and `?2 : U2 ~ V2`
+-- We also do this for the refl case
+
+-- We use some semantic information to add instantiations of coercions (particularly for functional dependencies)
+-- For example, if we know `u -> v` (u determines t) then we know that we ought to have three universally quantified
+-- variables: `u`, `v1`, and `v2` with the equation `v1 ~ v2`
+-- So, if we know that `F Int Bool` and `F Int b` then we "heuristically" produce the equation `Bool ~ b`
+-- I say heuristically because you could imagine searching the context and producing all possible instantiations
+
+-- For most situations when we are constructing instances it should be enough to just look at the available coercions
+-- without having to do instantiations in the context.
+
+-- The graph is constructed in the following way:
+-- A coercion `t : U ~ V` becomes two edges `U -> V` with label `t` and `V -> U` with label `sym t`
+-- A coercion `t : A B ~ C D` becomes two edges `A -> C` with label `t.1` and `B -> D` with label `t.2` (and of course the sym counterparts)
+-- A coercion `t : ∀ K, A ~ B` searches for all instantiations of `K` and adds coercions recursively from there.
+
+-- The instantiations are the worst issue here as it could theoretically explode.
+-- A depth-first search is then run on the graph and the labels of the graph are returned.
+-- These labels are joined by coercion sequencing: `t1 ; t2 ; ... ; tn` which produces the final coercion
