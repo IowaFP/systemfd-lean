901b26bc606697d443809d75331de59949087fe8
Apoorv Ingle <apoorv-ingle@uiowa.edu>
2025-11-21 17:19:07 -0600

    this semantic characterization of weakly deterministic value is probably bogus

--- SystemFD/Metatheory/SemSoundness.lean
+++ SystemFD/Metatheory/SemSoundness.lean
@@ -4,8 +4,8 @@ import SystemFD.Term.Variant
 import SystemFD.Metatheory.Canonicity
 import SystemFD.Metatheory.Confluence
 
-def saturated : Ctx Term -> Term -> Prop := sorry
 
+def saturated : Ctx Term -> Nat -> Prop := sorry
 -- TODO: Change Term.subexpr to Term.contains_variant decidable predicate
 
 inductive WDVal (Γ : Ctx Term) : Term -> Prop where
@@ -14,35 +14,62 @@ inductive WDVal (Γ : Ctx Term) : Term -> Prop where
      ¬ (Term.Subexpr `0 t) ->
      ∀ M N, ¬ (Term.Subexpr (M ⊕ N) t) ->
      ∀ M N P, ¬ (Term.Subexpr (.guard M P N) t) ->
-     (∀ x, Term.Subexpr #x t -> Γ.is_openm x -> saturated Γ #x) ->
+     (∀ x, Term.Subexpr #x t -> Γ.is_openm x -> saturated Γ x) ->
      WDVal Γ t
 
-
-inductive WDVal' (Γ : Ctx Term) : Term -> Prop where
-|  WDVal' :
-     ¬ (ContainsVariant (Ctx.variants Γ) [.zero, .guard, .ctor2 .choice] t) ->
-     (∀ x, Term.Subexpr #x t -> Γ.is_openm x -> saturated Γ #x) ->
-     -- cannot use ContainsVariant (Ctx.variants Γ) [.var .opent] becuase i want to imply that it is saturated.
-     WDVal' Γ t
+namespace Term
+def mk_ty_tm_app (t : Term) (τs : List Term) (es : List Term) := (t.mk_ty_apps τs).mk_apps es
+def GroundTy (Γ : Ctx Term) (t : Term) : Prop := sorry
+end Term
 
 
-inductive WDTerm : Ctx Term -> Term -> Prop where
+-- Semantic Characterization of a Weakly Deterministic Value
+inductive SemWDVal (Γ : Ctx Term) : Term ->  Prop where
+| SemWDVal :
+    ¬ (ContainsVariant (Ctx.variants Γ) [.zero, .guard, .ctor2 .choice] t) ->
+    (∀ x : Nat, Term.Subexpr #x t -> Γ.is_openm x ->
+    (∀ τs es,
+        (∀ e ∈ es, WDVal Γ e) ->
+        (∃ σ, Γ ⊢ (#x).mk_ty_tm_app τs es : σ ∧ σ.GroundTy Γ) ->
+        (¬ ((#x).mk_ty_tm_app τs es) ⟨ Γ ⟩⟶⋆ `0))) ->
+    (∃ v, t ⟨ Γ ⟩⟶⋆ v ∧ Val Γ v) ->
+    SemWDVal Γ t
+
+
+-- a. Semantically
+--    x is saturated iff
+--    ∀ τ e, x is open in Γ ->
+--    WDVal Γ e ->
+--    x[τ]e is of ground type ->
+--    ¬ (x[τ]e ⟶★ `0)
+inductive SemSat (Γ : Ctx Term) : Term -> Prop where
+| Sat :
+  t = #x ->
+  Γ.is_openm x ->
+  (∀ τs es,
+      (∀ e ∈ es, SemWDVal Γ e) ->
+      (∃ σ, Γ ⊢ (#x).mk_ty_tm_app τs es : σ ∧ σ.GroundTy Γ) ->
+      (¬ ((#x).mk_ty_tm_app τs es) ⟨ Γ ⟩⟶⋆ `0)) ->
+  SemSat Γ t
+
+
+inductive SemWDTerm (Γ : Ctx Term) : Term -> Prop where
 | WDBase :
-    (∃ v, (M ⟨ Γ ⟩⟶⋆ v) ∧  WDVal Γ v) ->
-    WDTerm Γ M
+    (∃ v, (M ⟨ Γ ⟩⟶⋆ v) ∧  Val Γ v) ->
+    SemWDTerm Γ M
 | WDChoice1 :
-   WDTerm Γ M ->
+   SemWDTerm Γ M ->
    (N ⟨ Γ ⟩⟶⋆ `0) ->
-   WDTerm Γ (M ⊕ N)
+   SemWDTerm Γ (M ⊕ N)
 | WDChoice2 :
-   WDTerm Γ N ->
+   SemWDTerm Γ N ->
    (M ⟨ Γ ⟩⟶⋆ `0) ->
-   WDTerm Γ (M ⊕ N)
+   SemWDTerm Γ (M ⊕ N)
 
 -- chain
 theorem sanity :
-  WDTerm Γ M ->
-  ∃ v, (M ⟨ Γ ⟩⟶⋆ v) ∧ WDVal Γ v := by
+  SemWDTerm Γ M ->
+  ∃ v, (M ⟨ Γ ⟩⟶⋆ v) ∧ Val Γ v := by
 intro h;
 induction h
 assumption
@@ -73,11 +100,18 @@ case _ N M h1 h2 ih =>
 -- a. Semantically
 --    x is saturated iff
 --    ∀ τ e, x is open in Γ ->
---    WDValue Γ e ->
+--    WDVal Γ e ->
 --    x[τ]e is of ground type ->
 --    x[τ]e -->* ∃ v. WDVal Γ v
 
 -- b. Syntactically
+--    AI: We don't have a syntactic characterization yet, maybe we never need it for our purposes.
+--    After all, when a programmer uses an overloaded operator, there is no indication that
+--    that function will actually be fine at runtime syntactically. It is only when the typechecker
+--    works on it (does some typed semantic analysis) is when it can generate an instance to fill in
+--    that decides what the behavior of the overloaded function is. Open term variables in our case
+--    _are_ the  overloaded functions
+
 -- c. Syntactic saturation => semantic saturation
 
 
