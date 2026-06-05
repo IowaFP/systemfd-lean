7e41b2b4254f0f727e84e1ddb6fa069223204d52
Andrew Marmaduke <marmaduke.andrew@gmail.com>
2026-06-05 16:05:36 -0500

    Reorg, focus only on Vec (#1)
    Add basic Vec operations, draft lemmas, add docs

--- Lilac/Vec/Basic.lean
+++ Lilac/Vec/Basic.lean
@@ -3,66 +3,259 @@ import Lilac.Vec.Encoding
 
 namespace Lilac
 
-inductive Vec (A : Sort u) : Nat -> Sort (imax u 1) where
-| nil : Vec A 0
-| cons : A -> Vec A n -> Vec A (n + 1)
+inductive Vec (α : Sort u) : Nat -> Sort (imax u 1) where
+| nil : Vec α 0
+| cons {n} : α -> Vec α n -> Vec α (n + 1)
 
-notation "#𝓋[]" => Vec.nil
+notation "#()" => Vec.nil
 infixr:67 (name := vec_cons) " :: " => Vec.cons
+
+protected def Vec.repr {α n} [Repr α] (v : Vec α n) (p : Nat) : Std.Format :=
+  "#(" ++ go v p ++ ")"
+where
+  go {α n} [Repr α] : Vec α n -> Nat -> Std.Format
+  | #(), _ => ""
+  | x::#(), p => repr x
+  | x::xs, p => repr x ++ "," ++ go xs p
+
+instance {n} {α : Type u} [Repr α] : Repr (Vec α n) where
+  reprPrec := Vec.repr
 
 -- Syntax adapted from Lean Prelude
 ----------------------------------------------------------------------------------------------------
 
-syntax (name := «term#𝓋[_,]») "#𝓋[" withoutPosition(term,*,?) "]" : term
-
+syntax (name := «term#(_,)») "#(" withoutPosition(term,*,?) ")" : term
+-- ⟨elems.getElems[i]!⟩
 open Lean in
 macro_rules
-  | `(#𝓋[ $elems,* ]) => do
-    -- NOTE: we do not have `TSepArray.getElems` yet at this point
-    let rec expandListLit (i : Nat) (skip : Bool) (result : TSyntax `term) : MacroM Syntax := do
+  | `(#( $elems,* )) => do
+    let rec expand_vec_lit (i : Nat) (skip : Bool) (result : TSyntax `term) : MacroM Syntax := do
       match i, skip with
-      | 0,   _     => pure result
-      | i+1, true  => expandListLit i false result
-      | i+1, false => expandListLit i true  (← ``(Vec.cons $(⟨elems.elemsAndSeps.get!Internal i⟩) $result))
+      | 0,     _     => pure result
+      | i + 1, true  => expand_vec_lit i false result
+      | i + 1, false => expand_vec_lit i true  (<- ``(Vec.cons $(⟨elems.elemsAndSeps[i]!⟩) $result))
     let size := elems.elemsAndSeps.size
     if size < 64 then
-      expandListLit size (size % 2 == 0) (← ``(Vec.nil))
+      expand_vec_lit size (size % 2 == 0) (<- ``(Vec.nil))
     else
       `(%[ $elems,* | Vec.nil ])
 
 @[app_unexpander Vec.nil]
 meta def Vec.unexpand_nil : Lean.PrettyPrinter.Unexpander
-| `($(_)) => `(#𝓋[])
+| `($(_)) => `(#())
 
 @[app_unexpander Vec.cons]
 meta def Vec.unexpand_cons : Lean.PrettyPrinter.Unexpander
 | `($(_) $x $tail) =>
   match tail with
-  | `([])      => `(#𝓋[$x])
-  | `([$xs,*]) => `(#𝓋[$x, $xs,*])
-  | `(⋯)       => `(#𝓋[$x, $tail])
+  | `([])      => `(#($x))
+  | `([$xs,*]) => `(#($x, $xs,*))
+  | `(⋯)       => `(#($x, $tail))
   | _          => throw ()
 | _ => throw ()
 
 ----------------------------------------------------------------------------------------------------
 
 @[coe]
-def Fun.Vec.to (v : Lilac.Fun.Vec A n) : Lilac.Vec A n :=
-  @induction A (λ n _ => Lilac.Vec A n) Lilac.Vec.nil (λ hd _ tl => Lilac.Vec.cons hd tl) _ v
+def Fun.Vec.to {α n} (v : Lilac.Fun.Vec α n) : Lilac.Vec α n :=
+  @induction α (λ n _ => Lilac.Vec α n) Lilac.Vec.nil (λ hd _ tl => Lilac.Vec.cons hd tl) _ v
 
 @[simp]
-theorem Fun.Vec.to_nil : to (nil : Vec A 0) = .nil := by rfl
-
-@[simp]
-theorem Fun.Vec.to_cons : to (cons hd tl) = .cons hd tl.to := by rfl
-
-@[simp]
-instance : Coe (Lilac.Fun.Vec A n) (Lilac.Vec A n) where
+instance {α n} : Coe (Lilac.Fun.Vec α n) (Lilac.Vec α n) where
   coe := Fun.Vec.to
 
 @[simp]
-def Vec.to : Vec A n -> Fun.Vec A n
-| nil => .nil
-| cons hd tl => .cons hd tl.to
+def Vec.to {α n} : Vec α n -> Fun.Vec α n
+| #() => .nil
+| x::xs => .cons x xs.to
+
+@[simp]
+def Vec.list {α n} : Vec α n -> List α
+| #() => []
+| x::xs => x::xs.list
+
+def Vec.has_dec_eq {α n} [DecidableEq α] : (a b : Vec α n) -> Decidable (a = b) := sorry
+
+instance {α n} [DecidableEq α] : DecidableEq (Vec α n) := Vec.has_dec_eq
+
+@[reducible, simp]
+def Vec.length {α n} (_ : Vec α n) : Nat := n
+
+@[simp]
+def Vec.head {α} : ∀ {n : Nat} [NeZero n], Vec α n -> α
+| 0, i, #() => i.out rfl |> False.elim
+| _, _, x::_ => x
+
+@[simp]
+def Vec.tail {α} : ∀ {n : Nat} [NeZero n], Vec α n -> Vec α (n - 1)
+| 0, i, #() => i.out rfl |> False.elim
+| _, _, _::xs => xs
+
+def Vec.get {α n} : Vec α n -> Fin n -> α
+| x::xs, i => Fin.cases x xs.get i
+
+instance {α n} : GetElem (Vec α n) (Fin n) α (λ _ _ => True) where
+  getElem xs i _ := Vec.get xs i
+
+def Vec.set {α n} : Vec α n -> Fin n -> α -> Vec α n
+| x::xs, i, a => Fin.cases (a::xs) (λ i => x::(xs.set i a)) i
+
+@[simp]
+def Vec.foldl {α : Type u} {β : Type v} {n} (f : α -> β -> α) (init : α) : Vec β n -> α
+| #() => init
+| x::xs => foldl f (f init x) xs
+
+@[simp]
+def Vec.foldr {α : Type u} {β : Type v} {n} (f : α -> β -> β) (init : β) : Vec α n -> β
+| #() => init
+| x::xs => f x (foldr f init xs)
+
+@[simp]
+def Vec.concat {α n} : Vec α n -> α -> Vec α (n + 1)
+| #(), a => a::#()
+| x::xs, a => x::(concat xs a)
+
+/--
+We intentionally flip the addition order to avoid `cast`ing and obtain better simplification
+-/
+@[simp]
+def Vec.append {α n m} : Vec α n -> Vec α m -> Vec α (m + n)
+| #(), ys => ys
+| x::xs, ys => x::(xs.append ys)
+
+instance {α : Type u} {n m : Nat} : HAppend (Vec α n) (Vec α m) (Vec α (m + n)) where
+  hAppend := Vec.append
+
+@[simp]
+def Vec.flatten {α n m} : Vec (Vec α n) m -> Vec α (n * m)
+| #() => #()
+| x::xs => x.append xs.flatten |> cast (by grind)
+
+@[simp]
+def Vec.map {α : Type u} {β : Type b} {n} (f : α -> β) : Vec α n -> Vec β n
+| #() => #()
+| x::xs => (f x)::(xs.map f)
+
+instance {n} : Functor (λ α => Vec α n) where
+  map := Vec.map
+
+@[simp]
+def Vec.beq {α n m} [BEq α] : Vec α n -> Vec α m -> Bool
+| #(), #() => true
+| x::xs, y::ys => x == y && beq xs ys
+| _, _ => false
+
+instance {α n} [BEq α] : BEq (Vec α n) := ⟨Vec.beq⟩
+
+@[simp]
+def Vec.replicate {α} : (n : Nat) -> (a : α) -> Vec α n
+| 0, _ => #()
+| n + 1, a => a::replicate n a
+
+@[simp, reducible]
+instance {α} : Inhabited (Vec α 0) where
+  default := #()
+
+@[simp, reducible]
+instance {α n} [Inhabited α] : Inhabited (Vec α n) where
+  default := Vec.replicate n default
+
+@[simp]
+def Vec.reverse {α n} : Vec α n -> Vec α n
+| #() => #()
+| x::xs => concat xs.reverse x
+
+def Vec.contains {α n} [BEq α] (a : α) : Vec α n -> Bool
+| #() => false
+| x::xs => x == a || contains a xs
+
+def Vec.take {α m} : (n : Nat) -> (h : n ≤ m) -> Vec α m -> Vec α n
+| _, h, #() => (#() : Vec α 0) |> cast (by cases h; simp)
+| 0, h, v => #()
+| n + 1, h, x::xs => x::xs.take n (by grind)
+
+def Vec.drop {α m} : (n : Nat) -> (h : n ≤ m) -> Vec α m -> Vec α (m - n)
+| _, h, #() => (#() : Vec α 0) |> cast (by cases h; simp)
+| 0, h, v => v
+| n + 1, h, x::xs => xs.drop n (by grind) |> cast (by grind)
+
+def Vec.any {α n} : Vec α n -> (p : α -> Bool) -> Bool
+| #(), _ => false
+| x::xs, p => p x || xs.any p
+
+def Vec.all {α n} : Vec α n -> (p : α -> Bool) -> Bool
+| #(), _ => true
+| x::xs, p => p x && xs.any p
+
+@[simp]
+def Vec.zipWith {α : Type u} {β : Type v} {γ : Type w} {n} (f : α -> β -> γ)
+  : Vec α n -> Vec β n -> Vec γ n
+| #(), #() => #()
+| x::xs, y::ys => f x y::zipWith f xs ys
+
+def Vec.zip {α : Type u} {β : Type v} {n} (v1 : Vec α n) (v2 : Vec β n) : Vec (α × β) n :=
+  zipWith (· , ·) v1 v2
+
+def Vec.unzip {α : Type u} {β : Type v} {n} : Vec (α × β) n -> (Vec α n) × (Vec β n)
+| #() => (#(), #())
+| (a, b)::xs =>
+  let (as, bs) := xs.unzip
+  (a::as, b::bs)
+
+@[simp]
+def Vec.range : (n : Nat) -> (k : Nat := 0) -> Vec Nat n
+| 0, _ => #()
+| n + 1, k => k::(range n (k + 1))
+
+@[simp]
+def Vec.zipIdx {α n} : Vec α n -> (k : Nat := 0) -> Vec (α × Nat) n
+| #(), _ => #()
+| x::xs, k => (x, k)::xs.zipIdx (k + 1)
+
+@[simp]
+def Vec.find? {α n} (p : α -> Bool) : Vec α n -> Option α
+| #() => none
+| x::xs => if p x then x else xs.find? p
+
+def Vec.findIdx? {α n} (p : α -> Bool) : Vec α n -> Option (Fin n)
+| #() => none
+| x::xs => go (x::xs) 0
+where
+  go : {n : Nat} -> Vec α n -> Nat -> Option (Fin n)
+  | _, #(), _ => none
+  | n + 1, x::xs, i => if p x then Fin.ofNat (n + 1) i else Fin.castSucc <$> go xs (i + 1)
+
+def Vec.erase {α} [BEq α] : {n : Nat} -> Vec α n -> α -> (m : Nat) × Vec α m ×' (n ≤ m + 1)
+| 0, #(), _ => ⟨0, #(), by grind⟩
+| n + 1, x::xs, a =>
+  match x == a with
+  | true => ⟨n, xs, by grind⟩
+  | false =>
+    let ⟨m, xs', h⟩ := erase xs a
+    ⟨m + 1, x::xs', by grind⟩
+
+@[simp]
+def Vec.count {α n} [BEq α] (a : α) : Vec α n -> Nat
+| #() => 0
+| x::xs => xs.count a + (if x == a then 1 else 0)
+
+@[simp]
+def Vec.traverse {m α β n} [Applicative m] (f : α -> m β) : Vec α n -> m (Vec β n)
+| #() => pure #()
+| x::xs => cons <$> f x <*> traverse f xs
+
+def Vec.sequence {m α n} [Applicative m] : Vec (m α) n -> m (Vec α n) := traverse id
+
+@[simp]
+def Vec.sum {n} : Vec Nat n -> Nat
+| #() => 0
+| x::xs => xs.sum + x
+
+inductive Vec.Mem {α} (x : α) : {n : Nat} -> Vec α n -> Prop where
+| head {xs} : Mem x (x::xs)
+| tail (y : α) {xs} : Mem x xs -> Mem x (y::xs)
+
+instance {α n} : Membership α (Vec α n) where
+  mem v x := Vec.Mem x v
 
 end Lilac
---
+++ Lilac/Vec/Lemma.lean
@@ -0,0 +1,386 @@
+
+import Lilac.Vec.Basic
+
+namespace Lilac
+
+/-!
+  # Vec Lemmas
+  TODO: Header
+-/
+
+/-! ## Fun.Vec.to -/
+
+@[simp]
+theorem Fun.Vec.to_nil {α} : to (nil : Vec α 0) = .nil := by rfl
+
+@[simp]
+theorem Fun.Vec.to_cons {α n hd} {tl : Vec α n} : to (cons hd tl) = .cons hd tl.to := by rfl
+
+@[simp]
+theorem Fun.Vec.to_iso {α n} {v : Fun.Vec α n} : v.to.to = v := sorry
+
+/-! ## to -/
+
+theorem Vec.to_iso {α n} {v : Vec α n} : v.to.to = v := sorry
+
+/-! ## list -/
+
+@[simp]
+theorem Vec.list_tail {α n} [NeZero n] {v : Vec α n} : v.tail.list = v.list.tail := sorry
+
+@[simp]
+theorem Vec.list_set {α n i x} {v : Vec α n} : (v.set i x).list = v.list.set i x := sorry
+
+@[simp]
+theorem Vec.list_concat {α n x} {v : Vec α n} : (v.concat x).list = v.list.concat x := sorry
+
+@[simp]
+theorem Vec.list_append {α n m} {v1 : Vec α n} (v2 : Vec α m) : (v1 ++ v2).list = v1.list ++ v2.list := sorry
+
+@[simp]
+theorem Vec.list_flatten {α n m} {v : Vec (Vec α n) m} : v.flatten.list = (list <$> v.list).flatten := sorry
+
+@[simp]
+theorem Vec.list_map {α β n} {f : α -> β} {v : Vec α n} : (v.map f).list = v.list.map f := sorry
+
+@[simp]
+theorem Vec.list_replicate {α n} {x : Nat} {v : Vec α n} : (replicate n x).list = List.replicate n x := sorry
+
+@[simp]
+theorem Vec.list_reverse {α n} {v : Vec α n} : v.reverse.list = v.list.reverse := sorry
+
+@[simp]
+theorem Vec.list_take {α n h} {v : Vec α n} : (v.take n h).list = v.list.take n := sorry
+
+@[simp]
+theorem Vec.list_drop {α : Type u} {m}
+  : {n : Nat} -> {v : Vec α m} -> {h : n ≤ m} -> (v.drop n h).list = v.list.drop n
+| _, #(), h => by cases h; simp [Vec.drop]
+| 0, x::xs, h => by simp [Vec.drop]
+| n + 1, x::xs, h => sorry
+
+@[simp]
+theorem Vec.list_zipWith {α β γ n} {f : α -> β -> γ} {v1 : Vec α n} {v2 : Vec β n}
+  : (zipWith f v1 v2).list = List.zipWith f v1.list v2.list
+:= sorry
+
+@[simp]
+theorem Vec.list_zip {α β n} {v1 : Vec α n} {v2 : Vec β n} : (zip v1 v2).list = List.zip v1.list v2.list := sorry
+
+@[simp]
+theorem Vec.list_range {n k} : (range n k).list = List.range' 0 n k := sorry
+
+@[simp]
+theorem Vec.list_zipIdx {α n k} {v : Vec α n} : (v.zipIdx k).list = v.list.zipIdx k := sorry
+
+@[grind .]
+theorem Vec.list_of_at_least_one_nonempty {α n} {v : Vec α (n + 1)} : v.list ≠ [] := sorry
+
+/-! ## length
+Vec carries its own length, hence why length is marked reducible.
+This also means that most operations on length are trivial and the simplifier will reject them.
+-/
+
+theorem Vec.length_to {α n} {v : Fun.Vec α n} : v.to.length = n := rfl
+
+@[simp]
+theorem Vec.length_tail {α n} [NeZero n] {v : Vec α n} : v.tail.length = n - 1 := rfl
+
+theorem Vec.length_set {α n i x} {v : Vec α n} : (v.set i x).length = n := rfl
+
+@[simp]
+theorem Vec.length_concat {α n x} {v : Vec α n} : (v.concat x).length = n + 1 := rfl
+
+@[simp]
+theorem Vec.length_append {α n m} {v1 : Vec α n} {v2 : Vec α m} : (v1 ++ v2).length = n + m := sorry
+
+@[simp]
+theorem Vec.length_flatten {α n m} {v : Vec (Vec α n) m} : v.flatten.length = n * m := rfl
+
+theorem Vec.length_map {α β n} {f : α -> β} {v : Vec α n} : (v.map f).length = n := rfl
+
+theorem Vec.length_reverse {α n} {v : Vec α n} : v.reverse.length = n := rfl
+
+theorem Vec.length_take {α m n h} {v : Vec α m} : (v.take n h).length = n := rfl
+
+@[simp]
+theorem Vec.length_drop {α m n h} {v : Vec α m} : (v.drop n h).length = m - n := rfl
+
+theorem Vec.length_zipWith {α β γ n} {f : α -> β -> γ} {v1 : Vec α n} {v2 : Vec β n} : (zipWith f v1 v2).length = n := rfl
+
+theorem Vec.length_zip {α β n} {v1 : Vec α n} {v2 : Vec β n} : (zip v1 v2).length = n := rfl
+
+theorem Vec.length_range {n k} : (range n k).length = n := rfl
+
+theorem Vec.length_zipIdx {α n k} {v : Vec α n} : (v.zipIdx k).length = n := rfl
+
+/-! ## head -/
+
+@[grind =]
+theorem Vec.head_zeroth_index_agree {α n} [NeZero n] {v : Vec α n} : v.head = v[Fin.ofNat n 0] := sorry
+
+@[simp]
+theorem Vec.head_set_zero {α n x} [NeZero n] {v : Vec α n} : (v.set 0 x).head = x := sorry
+
+@[simp]
+theorem Vec.head_set_succ {α n i x} [NeZero n] {v : Vec α n} : (v.set (i + 1) x).head = v.head := sorry
+
+@[simp]
+theorem Vec.head_concat {α n x} [NeZero n] {v : Vec α n} : (v.concat x).head = v.head := sorry
+
+@[simp]
+theorem Vec.head_append {n α m} [NeZero n] {v1 : Vec α n} (v2 : Vec α m) : (v1 ++ v2).head = v1.head := sorry
+
+@[simp]
+theorem Vec.head_flatten {α n m} [NeZero n] [NeZero m] {v : Vec (Vec α n) m} : v.flatten.head = v.head.head := sorry
+
+@[simp]
+theorem Vec.head_map {α β n} {f : α -> β} [NeZero n] {v : Vec α n} : (v.map f).head = f v.head := sorry
+
+@[simp]
+theorem Vec.head_replicate {α n} {x : α} [NeZero n] {v : Vec α n} : (replicate n x).head = x := sorry
+
+@[simp]
+theorem Vec.head_zipWith {α β γ n} {f : α -> β -> γ} [NeZero n] {v1 : Vec α n} {v2 : Vec β n} : (zipWith f v1 v2).head = f v1.head v2.head := sorry
+
+@[simp]
+theorem Vec.head_zip {α β n} [NeZero n] {v1 : Vec α n} {v2 : Vec β n} : (zip v1 v2).head = (v1.head, v2.head) := sorry
+
+@[simp]
+theorem Vec.head_range {n k} [NeZero n] : (range n k).head = k := sorry
+
+@[simp]
+theorem Vec.head_zipIdx {α n k} [NeZero n] {v : Vec α n} : (v.zipIdx k).head = (v.head, k) := sorry
+
+/-! ## tail -/
+
+-- TODO
+
+/-! ## get -/
+
+@[simp]
+theorem Vec.get_cons_zero {α n x} {xs : Vec α n} : (x::xs)[(0 : Fin (n + 1))] = x := sorry
+
+@[simp]
+theorem Vec.get_cons_succ {α n x} {xs : Vec α n} {i : Fin n} : (x::xs)[Fin.succ i] = xs[i] := sorry
+
+@[grind =]
+theorem Vec.get_set_neq {α n i j x} {v : Vec α n} (h : i ≠ j) : (v.set i x)[j] = v[j] := sorry
+
+@[simp]
+theorem Vec.get_set_eq {α n i x} {v : Vec α n} : (v.set i x)[i] = x := sorry
+
+@[grind =]
+theorem Vec.get_concat {α n x} {v : Vec α n} {i : Fin (n + 1)} (h : i ≠ 0) : (v.concat x)[i] = v[Fin.pred i h] := sorry
+
+@[grind =]
+theorem Vec.get_append_lt {α n m} {v1 : Vec α n} {v2 : Vec α m} {i : Fin (m + n)} (h : i < n) : (v1 ++ v2)[i] = v1[i.castLT h] := sorry
+
+theorem Vec.get_append_ge {α n m} {v1 : Vec α n} {v2 : Vec α m} {i : Fin (m + n)} (h : i ≥ n)
+  : (v1 ++ v2)[i] = v2[Fin.subNat n (i.cast (by grind) : Fin (m + n)) h]
+:= sorry
+
+@[simp]
+theorem Vec.get_map {α β n} {f : α -> β} {v : Vec α n} {i : Fin n} : (v.map f)[i] = f v[i] := sorry
+
+@[simp]
+theorem Vec.get_replicate {α n} {x : α} {i : Fin n} : (replicate n x)[i] = x := sorry
+
+@[simp]
+theorem Vec.get_reverse {α n} {v : Vec α n} {i : Fin n} : v.reverse[i] = v[i.rev] := sorry
+
+@[simp]
+theorem Vec.get_zipWith {α β γ n} {f : α -> β -> γ} {v1 : Vec α n} {v2 : Vec β n} {i : Fin n} : (zipWith f v1 v2)[i] = f v1[i] v2[i] := sorry
+
+@[simp]
+theorem Vec.get_zip {α β n} {v1 : Vec α n} {v2 : Vec β n} {i : Fin n} : (zip v1 v2)[i] = (v1[i], v2[i]) := sorry
+
+@[simp]
+theorem Vec.get_range {n k} {i : Fin n} : (range n k)[i] = i + k := sorry
+
+@[simp]
+theorem Vec.get_zipIdx {α n k} {v : Vec α n} {i : Fin n} : (v.zipIdx k)[i] = (v[i], i + k) := sorry
+
+/-! ## set -/
+
+-- TODO
+
+/-! ## foldl -/
+
+-- TODO
+
+/-! ## foldr -/
+
+-- TODO
+
+/-! ## concat -/
+
+-- TODO
+
+/-! ## append -/
+
+@[grind =]
+theorem Vec.append_nil_right {α n} {v : Vec α n} : v ++ (#() : Vec α 0) = (v |> cast (by simp)) := sorry
+
+@[grind =]
+theorem Vec.append_assoc {α n1 n2 n3} {v1 : Vec α n1} {v2 : Vec α n2} {v3 : Vec α n3}
+  : (v1 ++ v2) ++ v3 = (v1 ++ (v2 ++ v3) |> cast (by congr 1; grind))
+:= sorry
+
+/-! ## flatten -/
+
+-- TODO
+
+/-! ## map
+Why don't we use `Functor` / `LawfulFunctor`?
+Because Lean's type inference is not capable of figuring out the right functor `f` when
+using `Functor.map`.
+Try it yourself: implement `Functor (Vec · n)` and notice how `(· + 2) <$> #(1, 2)` fails.
+Solutions:
+1. Try to fix Lean's type inference algorithm, unclear if Lean developers will be receptive
+2. Use `Vec.map` directly (not horrible, `v.map f` is good ergonomics)
+3. Define our own `IndexedFunctor` type that will resolve the index correctly:
+```lean
+class IndexedFunctor (I : Type u) (F : Type v -> I -> Type w) where
+  imap {α β i} (f : α -> β) : F α i -> F β i
+
+instance : IndexedFunctor Nat Vec where
+  imap := Vec.map
+
+#eval IndexedFunctor.imap (· + 2) #(1, 2)
+```
+**For now we choose 2**
+-/
+
+@[simp, grind =]
+theorem Vec.map_id {v : Vec α n} : v.map id = v := sorry
+
+@[simp, grind =]
+theorem Vec.map_id_lambda {v : Vec α n} : v.map (λ x => x) = v := sorry
+
+@[simp, grind =]
+theorem Vec.map_tail {α β n} {f : α -> β} [NeZero n] {v : Vec α n} : v.tail.map f = (v.map f).tail := sorry
+
+@[simp, grind =]
+theorem Vec.map_set {α β n i x} {f : α -> β} {v : Vec α n} : (v.set i x).map f = (v.map f).set i (f x) := sorry
+
+@[simp, grind =]
+theorem Vec.map_concat {α β n x} {f : α -> β} {v : Vec α n} : (v.concat x).map f = (v.map f).concat (f x) := sorry
+
+@[simp, grind =]
+theorem Vec.map_append {α β n m} {f : α -> β} {v1 : Vec α n} {v2 : Vec α m} : (v1 ++ v2).map f = v1.map f ++ v2.map f := sorry
+
+@[simp, grind =]
+theorem Vec.map_flatten {α β n m} {f : α -> β} {v : Vec (Vec α n) m} : v.flatten.map f = (v.map (map f)).flatten := sorry
+
+@[simp, grind =]
+theorem Vec.map_map {α β γ n} {f : α -> β} {h : β -> γ} {v : Vec α n} : (v.map f).map h = v.map (h ∘ f) := sorry
+
+@[simp, grind =]
+theorem Vec.map_replicate {α β n} {a : α} {f : α -> β} : (replicate n a).map f = replicate n (f a) := sorry
+
+@[simp, grind =]
+theorem Vec.map_reverse {α β n} {f : α -> β} {v : Vec α n} : v.reverse.map f = (v.map f).reverse := sorry
+
+@[simp, grind =]
+theorem Vec.map_take {α β m n} {f : α -> β} {h} {v : Vec α m} : (v.take n h).map f = (v.map f).take n h := sorry
+
+@[simp, grind =]
+theorem Vec.map_drop {α β m n} {f : α -> β} {h} {v : Vec α m} : (v.drop n h).map f = (v.map f).drop n h := sorry
+
+/-! ## beq -/
+
+instance {α n} [BEq α] [ReflBEq α] : ReflBEq (Vec α n) where
+  rfl := sorry
+
+instance {α n} [BEq α] [LawfulBEq α] : LawfulBEq (Vec α n) where
+  eq_of_beq := sorry
+
+@[grind =]
+theorem Vec.beq_head_tail {α n} [BEq α] [NeZero n] {v1 : Vec α n} {v2 : Vec α n} : v1.head == v2.head ∧ v1.tail == v2.tail <-> v1 == v2 := sorry
+
+-- TODO: there is some funext automation thing that should be used here
+@[grind <-]
+theorem Vec.beq_get {α n} [BEq α] {v1 : Vec α n} {v2 : Vec α n} (h : ∀ (i : Fin n), v1[i] == v2[i]) : v1 == v2 := sorry
+
+/-! ## replicate -/
+
+-- TODO
+
+/-! ## reverse -/
+
+-- TODO
+
+/-! ## contains -/
+
+-- TODO
+
+/-! ## take -/
+
+-- TODO
+
+/-! ## drop -/
+
+-- TODO
+
+/-! ## any -/
+
+-- TODO
+
+/-! ## all -/
+
+-- TODO
+
+/-! ## zipWith -/
+
+-- TODO
+
+/-! ## zip -/
+
+-- TODO
+
+/-! ## unzip -/
+
+-- TODO
+
+/-! ## range -/
+
+-- TODO
+
+/-! ## zipIdx -/
+
+-- TODO
+
+/-! ## find? -/
+
+-- TODO
+
+/-! ## findIdx? -/
+
+-- TODO
+
+/-! ## erase -/
+
+-- TODO
+
+/-! ## count -/
+
+-- TODO
+
+/-! ## traverse -/
+
+-- TODO
+
+/-! ## sequence -/
+
+-- TODO
+
+/-! ## sum -/
+
+-- TODO
+
+/-! ## Mem -/
+
+-- TODO
+
+end Lilac
