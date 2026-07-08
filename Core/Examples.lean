d887474a39e9437e93e1ee8496c2a66486e652cd
Apoorv Ingle <apoorv-ingle@uiowa.edu>
2026-07-08 16:46:52 -0500

    - back references to the paper - complete class alias example

--- Core/Examples.lean
+++ Core/Examples.lean
@@ -1,8 +1,10 @@
-import Core.Examples.Boolean
-import Core.Examples.Ordering  --- § 2.1
-import Core.Examples.AlternativeMonoid
-import Core.Examples.MonadReaderRelation
-import Core.Examples.MonadReaderTypeFunction
+import Core.Examples.Boolean  --- § 2.1.1 Superclasses
+import Core.Examples.Ordering  --- § 2.1 Open Data and Open Function
+import Core.Examples.Superclasses  --- § 2.2.1 Superclasses, § 2.2.3 Default implementation and local instances
+import Core.Examples.AlternativeMonoid -- § 2.2.2 Quantified superclasses
+import Core.Examples.ClassAlias        -- § 2.3.1 Generalized superclasses and class aliases
+import Core.Examples.MonadReaderRelation -- § 2.3.2 Type Relationships
+import Core.Examples.MonadReaderTypeFunction -- § 2.4 Open Functions and Coercions
 
 import Core.Examples.Maybe
 import Core.Examples.TypeFunction
--- Core/Examples/ClassAlias.lean
+++ Core/Examples/ClassAlias.lean
@@ -15,13 +15,28 @@ open LeanSubst
 namespace Core.Examples.ClassAlias
 
 def CACtx : GlobalEnv := [
+
+  .inst "mpRequirement" #(⟨"ListMon", 1, #(t#0), 0, 1⟩, ⟨"ListAlt", 1, #(t#0), 0, 1⟩) (inst! "ListMonPlus" #(t#0) #() #(#0).to),
+  .octor "ListMonPlus" ⟨1, #(★ -:> ★), 0, #(), 1, #(t#0 ~[★ -:> ★]~ gt#"List"), gt#"MonadPlus" • t#0⟩,
+  .octor "ListAlt" ⟨1, #(★ -:> ★), 0, #(), 1, #(t#0 ~[★ -:> ★]~ gt#"List"), gt#"Alternative" • t#0⟩,
+  .octor "ListMon" ⟨1, #(★ -:> ★), 0, #(), 1, #(t#0 ~[★ -:> ★]~ gt#"List"), gt#"Monad" • t#0⟩,
+
+  -- List ★ → ★ where
+  --   Nil : ∀ α. List α
+  --   Cons : ∀ α. α → List α → List α
+  .data 2 "List" (★ -:> ★)
+      #( ("Nil", ⟨1, #(★), 0, #(), 0, #(), (gt#"List" • t#0)⟩),
+         ("Cons", ⟨1, #(★), 0, #(), 2,  #(t#0, gt#"List" • t#0), (gt#"List" • t#0)⟩) ),
+
+  -- mpRequirement : ∀ t. Monad t, Alternative t ⇒ MonadPlus t
   .openm "mpRequirement" ⟨1, #(★ -:> ★), 0, #(), 2, #(gt#"Monad" • t#0, gt#"Alternative" • t#0), gt#"MonadPlus" • t#0⟩,
   -- class MonadPlus (m : ★ → ★)
   .odata "MonadPlus" ((★ -:> ★) -:> ★),
   -- class Alternative (m : ★ → ★)
   .odata "Alternative" ((★ -:> ★) -:> ★),
   -- class Monad (m : ★ → ★)
-  .odata "Monad" ((★ -:> ★) -:> ★) ]
+  .odata "Monad" ((★ -:> ★) -:> ★) ] ++ CastCtx
+
 
 #guard CACtx.wf_globals == some ()
 #guard CACtx.check_open_exhaustive == some ()
